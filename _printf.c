f0ca2afe646b59b598dbefa378fc29c86cfab01d
tasneem04 <eng.wr.tasneem@gmail.com>
2023-10-17 05:24:11 -0700

    Fix

--- _printf.c
+++ _printf.c
@@ -4,40 +4,47 @@
 
 int _printf(const char *format, ...)
 {
- va_list args;
+  va_list args;
     va_start(args, format);
 
-    int count = 0;
-    while (*format != '\0')
-    {
-        if (*format != '%')
-        {
-            putchar(*format);
-            count++;
-        }
-        else
-        {
-            // Handle format specifiers
-            switch (*++format)
-          {
-            case 'c':
-                count += fprintf(stdout, "%c", va_arg(args, char));
-                break;
-            case 's':
-                count += fprintf(stdout, "%s", va_arg(args, char *));
-                break;
-          
-            default:
-                // Handle unknown format specifiers
-                putchar('%');
-                putchar(*format);
-                count += 2;
-                break;
+    int char_count = 0; // To keep track of the number of characters printed
+
+    while (*format != '\0') {
+        if (*format == '%') {
+            format++; // Move past the '%'
+            switch (*format) {
+                case 'c':
+                    // Handle %c specifier (character)
+                    char c = va_arg(args, int); // Characters are promoted to int
+                    putchar(c);
+                    char_count++;
+                    break;
+                case 's':
+                    // Handle %s specifier (string)
+                    char *str = va_arg(args, char *);
+                    fputs(str, stdout);
+                    char_count += strlen(str);
+                    break;
+                case '%':
+                    // Handle %% as a literal '%'
+                    putchar('%');
+                    char_count++;
+                    break;
+                default:
+                    // Unsupported format specifier, print it as is
+                    putchar('%');
+                    char_count++;
+                    putchar(*format);
+                    char_count++;
             }
+        } else {
+            // Print characters that are not format specifiers
+            putchar(*format);
+            char_count++;
         }
         format++;
     }
 
     va_end(args);
-    return count;
+    return char_count; // Return the total number of characters printed
 }
