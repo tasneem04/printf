e9c5b569442091cbc6775c398a708e151adfe941
tasneem04 <eng.wr.tasneem@gmail.com>
2023-10-17 14:21:39 -0700

    Fix printf

---
+++ _printf.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "main.h"
+
+
+int _printf(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+
+    int count = 0;
+    while (*format != '\0')
+    {
+        if (*format != '%')
+        {
+            putchar(*format);
+            count++;
+        }
+        else
+        {
+            // Handle format specifiers
+            switch (*++format)
+            {
+            case 'd':
+                count += fprintf(stdout, "%d", va_arg(args, int));
+                break;
+            case 'c':
+                count += fprintf(stdout, "%c", va_arg(args, char));
+                break;
+            case 's':
+                count += fprintf(stdout, "%s", va_arg(args, char *));
+                break;
+            case 'f':
+                count += fprintf(stdout, "%f", va_arg(args, double));
+                break;
+            default:
+                // Handle unknown format specifiers
+                putchar('%');
+                putchar(*format);
+                count += 2;
+                break;
+            }
+        }
+        format++;
+    }
+
+    va_end(args);
+    return count;
+}
+
