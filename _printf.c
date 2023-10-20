1b2c1f0d99d020503bb28a9cd92b78f7b3102890
tasneem04 <eng.wr.tasneem@gmail.com>
2023-10-20 04:22:28 -0700

    Fix

--- _printf.c
+++ _printf.c
@@ -13,56 +13,46 @@
 
 int _printf(const char *format, ...)
 {
- char token[1000];
- int k = 0, i;   
- va_list ptr;
-    va_start(ptr, format);
+	int count = 0;
+ va_list args;
+    va_start(args, format);
 
-    for (i = 0; format[i] != '\0'; i++)
+  
+    while (*format != '\0')
     {
-        token[k++] = format[i];
-
-        if (format[i + 1] == '%' || format[i + 1] == '\0')
-       	{
-            token[k] = '\0';
-            k = 0;
-            if (token[0] != '%')
-	    {
-                fprintf(stdout, "%s", token);
-            } else {
-                int j = 1;
-                char ch1 = 0;
-
-                while ((ch1 = token[j++]) < 58) {
-                }
-                if (ch1 == 'i' || ch1 == 'd' || ch1 == 'u' || ch1 == 'h')
-	       	{
-                    fprintf(stdout, token, va_arg(ptr, int));
-                } else if (ch1 == 'c') {
-                    fprintf(stdout, token, va_arg(ptr, int));
-                } else if (ch1 == 'f') {
-                    fprintf(stdout, token, va_arg(ptr, double));
-                } else if (ch1 == 'l') {
-                    char ch2 = token[2];
-                    if (ch2 == 'u' || ch2 == 'd' || ch2 == 'i') {
-                        fprintf(stdout, token, va_arg(ptr, long));
-                    } else if (ch2 == 'f') {
-                        fprintf(stdout, token, va_arg(ptr, double));
-                    }
-                } else if (ch1 == 'L') {
-                    char ch2 = token[2];
-                     if (ch2 == 'f') {
-                        fprintf(stdout, token, va_arg(ptr, long double));
-                    }
-                } else if (ch1 == 's') {
-                    fprintf(stdout, token, va_arg(ptr, char*));
-                } else {
-                    fprintf(stdout, "%s", token);
-                }
+        if (*format != '%')
+        {
+            putchar(*format);
+            count++;
+        }
+        else
+        {
+           
+            switch (*++format)
+            {
+            case 'd':
+                count += fprintf(stdout, "%d", va_arg(args, int));
+                break;
+            case 'c':
+                count += fprintf(stdout, "%c", (char)va_arg(args, int));
+		break;
+            case 's':
+                count += fprintf(stdout, "%s", va_arg(args, char *));
+                break;
+            case 'f':
+                count += fprintf(stdout, "%f", va_arg(args, double));
+                break;
+            default:
+                
+                putchar('%');
+                putchar(*format);
+                count += 2;
+                break;
             }
         }
+        format++;
     }
 
-    va_end(ptr);
-    return 0;
-}	
+    va_end(args);
+    return count;
+}  
