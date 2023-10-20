c97500014f2b8910432b04543c1f4e549ee9618f
tasneem04 <eng.wr.tasneem@gmail.com>
2023-10-20 04:16:34 -0700

    Fix

--- _printf.c
+++ _printf.c
@@ -13,47 +13,56 @@
 
 int _printf(const char *format, ...)
 {
-	int count = 0;
- va_list args;
-    va_start(args, format);
+ char token[1000];
+ int k = 0, i;   
+ va_list ptr;
+    va_start(ptr, format);
 
-  
-    while (*format != '\0')
+    for (i = 0; format[i] != '\0'; i++)
     {
-        if (*format != '%')
-        {
-            putchar(*format);
-            count++;
-        }
-        else
-        {
-           
-            switch (*++format)
-            {
-            case 'd':
-                count += fprintf(stdout, "%d", va_arg(args, int));
-                break;
-            case 'c':
-                count += fprintf(stdout, "%c", (char)va_arg(args, int));
-		break;
-            case 's':
-                count += fprintf(stdout, "%s", va_arg(args, char *));
-                break;
-            case 'f':
-                count += fprintf(stdout, "%f", va_arg(args, double));
-                break;
-            default:
-                
-                putchar('%');
-                putchar(*format);
-                count += 2;
-                break;
+        token[k++] = format[i];
+
+        if (format[i + 1] == '%' || format[i + 1] == '\0')
+       	{
+            token[k] = '\0';
+            k = 0;
+            if (token[0] != '%')
+	    {
+                fprintf(stdout, "%s", token);
+            } else {
+                int j = 1;
+                char ch1 = 0;
+
+                while ((ch1 = token[j++]) < 58) {
+                }
+                if (ch1 == 'i' || ch1 == 'd' || ch1 == 'u' || ch1 == 'h')
+	       	{
+                    fprintf(stdout, token, va_arg(ptr, int));
+                } else if (ch1 == 'c') {
+                    fprintf(stdout, token, va_arg(ptr, int));
+                } else if (ch1 == 'f') {
+                    fprintf(stdout, token, va_arg(ptr, double));
+                } else if (ch1 == 'l') {
+                    char ch2 = token[2];
+                    if (ch2 == 'u' || ch2 == 'd' || ch2 == 'i') {
+                        fprintf(stdout, token, va_arg(ptr, long));
+                    } else if (ch2 == 'f') {
+                        fprintf(stdout, token, va_arg(ptr, double));
+                    }
+                } else if (ch1 == 'L') {
+                    char ch2 = token[2];
+                     if (ch2 == 'f') {
+                        fprintf(stdout, token, va_arg(ptr, long double));
+                    }
+                } else if (ch1 == 's') {
+                    fprintf(stdout, token, va_arg(ptr, char*));
+                } else {
+                    fprintf(stdout, "%s", token);
+                }
             }
         }
-        format++;
     }
 
-    va_end(args);
-    return count;
-}  
-                  
+    va_end(ptr);
+    return 0;
+}	
