2ba1192ccc8853176775acf475ab0421e017538f
tasneem04 <eng.wr.tasneem@gmail.com>
2023-10-20 03:38:48 -0700

    Fix the varibale arrange

--- _printf.c
+++ _printf.c
@@ -11,10 +11,10 @@
  * Return: The number of characters printed
  */
 int _printf(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
 
-    int char_count = 0; // To keep track of the number of characters printed
+      int char_count = 0;
+va_list args;
+    va_start(args, format);
 
     while (*format) {
         if (*format == '%') {
@@ -22,36 +22,36 @@ int _printf(const char *format, ...) {
             switch (*format) {
                 case 'c': {
                     char c = va_arg(args, int);
-                    write(1, &c, 1); // Print character
+                    write(1, &c, 1);
                     char_count++;
                     break;
                 }
                 case 's': {
                     char *str = va_arg(args, char *);
                     while (*str) {
-                        write(1, str, 1); // Print each character in the string
+                        write(1, str, 1);
                         str++;
                         char_count++;
                     }
                     break;
                 }
                 case '%':
-                    write(1, "%", 1); // Print a literal '%'
+                    write(1, "%", 1);
                     char_count++;
                     break;
                 default:
-                    write(1, "%", 1); // Print the '%' and the unsupported character
+                    write(1, "%", 1);
                     char_count++;
                     write(1, format, 1);
                     char_count++;
             }
         } else {
-            write(1, format, 1); // Print characters that are not format specifiers
+            write(1, format, 1);
             char_count++;
         }
         format++;
     }
 
     va_end(args);
-    return char_count; // Return the total number of characters printed
+    return char_count;
 }
